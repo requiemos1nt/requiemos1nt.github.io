112ea9578a6d4059ca15add5b51cd56536238c1f
requiemos1nt <requiem888work@gmail.com>
2025-03-22 03:36:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,9 @@ const images = document.querySelectorAll(".gallery img");
 const dots = document.querySelectorAll(".dot");
 const screenContainer = document.querySelector(".screen-container");
 
-
 function updateBackground(imageSrc) {
     screenContainer.style.backgroundImage = `url(${imageSrc})`;
 }
-
-updateBackground(images[index].src);
-
 
 function updateDots() {
     dots.forEach((dot, i) => {
@@ -19,17 +15,15 @@ function updateDots() {
 }
 
 function showImage(newIndex) {
-    images[index].classList.remove("active");
+    if (newIndex < 0 || newIndex >= images.length) return;
+
+    images.forEach(img => img.classList.remove("active"));
     index = newIndex;
     images[index].classList.add("active");
 
-   
     updateDots();
-
-   
     updateBackground(images[index].src);
 }
-
 
 document.querySelector(".gallery").addEventListener("touchstart", (e) => {
     startX = e.touches[0].clientX;
@@ -37,12 +31,28 @@ document.querySelector(".gallery").addEventListener("touchstart", (e) => {
 
 document.querySelector(".gallery").addEventListener("touchend", (e) => {
     let endX = e.changedTouches[0].clientX;
-    if (startX - endX > 50 && index < images.length - 1) {
+    if (startX - endX > 50) showImage(index + 1);
+    else if (startX - endX < -50) showImage(index - 1);
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") showImage(index + 1);
+    else if (e.key === "ArrowLeft") showImage(index - 1);
+});
+
+document.querySelector(".gallery").addEventListener("click", (e) => {
+    const mid = e.currentTarget.clientWidth / 2;
+    if (e.clientX > e.currentTarget.getBoundingClientRect().left + mid) {
         showImage(index + 1);
-    } else if (startX - endX < -50 && index > 0) {
+    } else {
         showImage(index - 1);
     }
 });
 
+// Обработка кликов по точкам
+dots.forEach((dot, i) => {
+    dot.addEventListener("click", () => showImage(i));
+});
 
+updateBackground(images[index].src);
 updateDots();
